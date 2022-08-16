a09a07145157da49b98319c3f16249b80753246c
Ari T <60534107+fbzari@users.noreply.github.com>
2022-08-16 18:27:01 +0530

    Update api.service.ts
    change api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,16 +9,16 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   postProduct(data : any){
-    return this.http.post<any>("http://localhost:3000/productList/",data);
+    return this.http.post<any>("https://crudcrud.com/api/57b6f1dd35544d62ae0bab954652be8d",data);
   }
   getProduct(){
-    return this.http.get<any>("http://localhost:3000/productList/");
+    return this.http.get<any>("https://crudcrud.com/api/57b6f1dd35544d62ae0bab954652be8d");
   }
   putProduct(data : any,id : number){
-    return this.http.put<any>("http://localhost:3000/productList/"+id ,data);
+    return this.http.put<any>("https://crudcrud.com/api/57b6f1dd35544d62ae0bab954652be8d"+id ,data);
   } 
   deleteProduct(id : number){
-    return this.http.delete<any>("http://localhost:3000/productList/"+id);
+    return this.http.delete<any>("https://crudcrud.com/api/57b6f1dd35544d62ae0bab954652be8d"+id);
   } 
 }
   
